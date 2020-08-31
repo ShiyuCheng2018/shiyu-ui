fdc3327ac94b9c2e17d89031962ebdd04095aa08
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-08-30 22:53:54 -0700

    feat(src/componnets/button): button attributes
    button attributes: React.ButtoneHTMLAttributes<HTMLElement>

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -22,10 +22,24 @@ interface BaseButtonProps {
     href?: string;
 }
 
-const Button: React.FC<BaseButtonProps> = (props) => {
-    const { btnType, disabled, size, children, href } = props;
+type NativeButtonProps = BaseButtonProps &
+    React.ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = BaseButtonProps &
+    React.AnchorHTMLAttributes<HTMLElement>;
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
 
-    const classes = classNames("btn", {
+const Button: React.FC<ButtonProps> = (props) => {
+    const {
+        btnType,
+        className,
+        disabled,
+        size,
+        children,
+        href,
+        ...restProps
+    } = props;
+
+    const classes = classNames("btn", className, {
         [`btn-${btnType}`]: btnType,
         [`btn-${size}`]: size,
         disabled: btnType === ButtonType.Link && disabled,
@@ -33,13 +47,13 @@ const Button: React.FC<BaseButtonProps> = (props) => {
 
     if (btnType === ButtonType.Link && href) {
         return (
-            <a className={classes} href={href}>
+            <a className={classes} href={href} {...restProps}>
                 {children}
             </a>
         );
     } else {
         return (
-            <button className={classes} disabled={disabled}>
+            <button className={classes} disabled={disabled} {...restProps}>
                 {children}
             </button>
         );
