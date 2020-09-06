1993f85f1eb0e9e28d4efdbb36ac3025fddb730d
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-09-06 02:47:27 -0700

    feat(src/components/progress): configured Progress component
    configured Progress component

--- src/components/Upload/upload.stories.tsx
+++ src/components/Upload/upload.stories.tsx
@@ -31,7 +31,7 @@ const filePromise = (file: File) => {
 const SimpleUpload = () => {
     return (
         <Upload
-            action={"https//jsonplaceholder.typicode.com/post/"}
+            action={"https://www.mocky.io/v2/5cc8019d300000980a055e76"}
             onProgress={action("progress")}
             onSuccess={action("success")}
             onError={action("error")}
