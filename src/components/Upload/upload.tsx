75eafbca89880780a856f643f4f440b5d5c105b8
Shiyu Cheng <starkeyshiyu@gmail.com>
2020-09-06 00:14:34 -0700

    feat(src/upload): upload component
    upload component

--- src/components/Upload/upload.tsx
+++ src/components/Upload/upload.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useRef } from "react";
+import React, { ChangeEvent, FC, useRef } from "react";
 import axios from "axios";
 import Button from "../Button/button";
 
@@ -11,9 +11,70 @@ export interface UploadProps {
 
 export const Upload: FC<UploadProps> = (props) => {
     const { action, onProgress, onSuccess, onError } = props;
+    const fileInput = useRef<HTMLInputElement>(null);
+
+    const handleClick = () => {
+        if (fileInput.current) {
+            fileInput.current.click();
+        }
+    };
+
+    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const files = e.target.files;
+        if (!files) {
+            return;
+        }
+        uploadFiles(files);
+        if (fileInput.current) {
+            fileInput.current.value = "";
+        }
+    };
+
+    const uploadFiles = (files: FileList) => {
+        let postFiles = Array.from(files);
+        postFiles.forEach((file) => {
+            const formData = new FormData();
+            formData.append(file.name, file);
+            axios
+                .post(action, formData, {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                    onUploadProgress: (e) => {
+                        let percentage =
+                            Math.round((e.loaded * 100) / e.total) || 0;
+                        if (percentage < 100) {
+                            if (onProgress) {
+                                onProgress(percentage, file);
+                            }
+                        }
+                    },
+                })
+                .then((res) => {
+                    if (onSuccess) {
+                        onSuccess(res.data, file);
+                    }
+                })
+                .catch((err) => {
+                    if (onError) {
+                        onError(err, file);
+                    }
+                });
+        });
+    };
+
     return (
         <div className={"shiyu-upload-component"}>
-            <Button btnType={"primary"}>Upload File</Button>
+            <Button btnType={"primary"} onClick={handleClick}>
+                Upload File
+            </Button>
+            <input
+                className="shiyu-file-input"
+                style={{ display: "none" }}
+                ref={fileInput}
+                onChange={handleFileChange}
+                type="file"
+            />
         </div>
     );
 };
